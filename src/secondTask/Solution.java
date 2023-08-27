7fbd0bb2a85372787482b2d8ec640dbdcc7c612c
Andrey Nachevny <nochergt@gmail.com>
2023-08-27 21:02:36 +0300

    add psvm

--- src/secondTask/Solution.java
+++ src/secondTask/Solution.java
@@ -2,10 +2,16 @@ package secondTask;
 
 public class Solution {
 
-    public void replace(int a, int b){
+    public static void replace(int a, int b){
         a +=b;
         b = a -b;
         a -=b;
         System.out.println("a - "+ a + ", b - " + b);
+    }
+
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 10;
+        Solution.replace(a,b);
     }
 }
--- src/thirdTask/Solution.java
+++ src/thirdTask/Solution.java
@@ -1,9 +1,17 @@
 package thirdTask;
 
 public class Solution {
-    public boolean isPalindrome(String string){
+    public static boolean isPalindrome(String string){
         StringBuilder palindrome = new StringBuilder(string);
         palindrome.reverse();
         return string.contentEquals(palindrome);
     }
+
+    public static void main(String[] args) {
+        String palindrome = "kazak";
+        String nonPalindrome = "greenAtom";
+        System.out.println(Solution.isPalindrome(palindrome));
+        System.out.println(Solution.isPalindrome(nonPalindrome));
+    }
+
 }
